c034a2cf90bef30a0fcdacdf4624f1cce68886a8
Fr3ud <oo.pavelko@gmail.com>
2021-02-24 19:57:33 +0200

    feat: add basic markup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-console.log('Hello World!');
+const rootElement = document.getElementById('root');
+
+const AppHeader = () => (<h1>To-Do List</h1>);
+const SearchPanel = () => (<input placeholder="search" />);
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    );
+};
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
