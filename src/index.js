a6bc60d941122571b937f711b20c95d04a037db3
Fr3ud <oo.pavelko@gmail.com>
2021-02-24 20:16:55 +0200

    feat: update component

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ const rootElement = document.getElementById('root');
 const AppHeader = () => (<h1>To-Do List</h1>);
 const SearchPanel = () => (<input placeholder="search" />);
 const TodoList = () => {
+    const items = ['Learn React', 'Build Awesome App'];
+
     return (
         <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
+            <li>{ items[0] }</li>
+            <li>{ items[1] }</li>
         </ul>
     );
 };
