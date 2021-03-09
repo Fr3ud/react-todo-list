e0004c8e7704e38fbbe8138cf065fad2d0d0f993
Fr3ud <oo.pavelko@gmail.com>
2021-03-09 15:21:28 +0200

    feat: add app component

--- src/index.js
+++ src/index.js
@@ -1,34 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import ItemStatusFilter from './components/item-status-filter';
-import TodoList from './components/todo-list';
-
-import './index.css';
+import App from './components/app';
 
 const rootElement = document.getElementById('root');
-
-const App = () => {
-    const todoData = [
-        { id: 1, label: 'Drink Coffee', important: false },
-        { id: 2, label: 'Build React App', important: true },
-        { id: 3, label: 'Have a lunch', important: false },
-    ];
-
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
-            </div>
-            <TodoList todos={todoData} />
-        </div>
-    );
-};
 
 ReactDOM.render(<App />, rootElement);
 
